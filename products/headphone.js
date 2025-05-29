1dfc825ca2184ec2d339655d1a7492db763eeec2
G.Naga Abhishek <145243247+nagaabhi@users.noreply.github.com>
2025-05-29 09:52:35 +0530

    Update headphone.js

--- products/headphone.js
+++ products/headphone.js
@@ -2,42 +2,42 @@ export const headphones = [
   {
     name: "Sony WH-1000XM5",
     price: 349.99,
-    image: "/shop-head-phones/images/head-phone1.png"
+    image: "./images/head-phone1.png"
   },
   {
     name: "Bose QuietComfort 45",
     price: 279.00,
-    image: "/shop-head-phones/images/head-phone2.png"
+    image: "./images/head-phone2.png"
   },
   {
     name: "Apple AirPods Max",
     price: 549.00,
-    image: "/shop-head-phones/images/head-phone3.png"
+    image: "./images/head-phone3.png"
   },
   {
     name: "Sennheiser HD 660S2",
     price: 599.95,
-    image: "/shop-head-phones/images/head-phone4.png"
+    image: "./images/head-phone4.png"
   },
   {
     name: "Sony WH-1000XM5",
     price: 349.99,
-    image: "/shop-head-phones/images/head-phone1.png"
+    image: "./images/head-phone1.png"
   },
   {
     name: "Bose QuietComfort 45",
     price: 279.00,
-    image: "/shop-head-phones/images/head-phone2.png"
+    image: "./images/head-phone2.png"
   },
   {
     name: "Apple AirPods Max",
     price: 549.00,
-    image: "/shop-head-phones/images/head-phone3.png"
+    image: "./images/head-phone3.png"
   },
   {
     name: "Sennheiser HD 660S2",
     price: 599.95,
-    image: "/shop-head-phones/images/head-phone4.png"
+    image: ".//images/head-phone4.png"
   },
 
 ];
